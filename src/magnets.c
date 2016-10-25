253ea909c6fef53bec67280248990bdb7529cdda
dosnerd <dosprez@avans.nl>
2016-10-25 13:55:22 +0200

    SWITCH TO RIGHT PINS
    CHANGE		switch to port 2

--- src/magnets.c
+++ src/magnets.c
@@ -4,30 +4,35 @@
  *  Created on: 10 okt. 2016
  *      Author: Acer
  */
-#define MAGNET0 0x00400000
+#define MAGNET0 0x40
+#define MAGNET1 0x80
 
-#define FIO0DIR (*(unsigned int *)0x2009C000)	//GPIO direction
-#define FIO0SET (*(unsigned int *)0x2009C018)	//GPIO set (can make pin high)
-#define FIO0CLR (*(unsigned int *)0x2009C01C)	//GPIO clear (can make pin low)
-#define FIO0PIN (*(unsigned int *)0x2009C014) 	//GPIO value (can read and write pin) (write careful! Will effect whole port)
-#define IO0IntEnR (*(unsigned int *)0x40028090)	//GPIO interrupt set rising edge
-#define IO0IntClr (*(unsigned int *)0x4002808C)	//GPIO interupt clear
+//48, 53
+
+#define FI20DIR (*(unsigned int *)0x2009C040)	//GPIO direction
+#define FIO2SET (*(unsigned int *)0x2009C058)	//GPIO set (can make pin high)
+#define FIO2CLR (*(unsigned int *)0x2009C05C)	//GPIO clear (can make pin low)
+#define FIO2PIN (*(unsigned int *)0x2009C054) //GPIO value (can read and write pin) (write careful! Will effect whole port)#define IO0IntEnR (*(unsigned int *)0x40028090)	//GPIO interrupt set rising edge
+#define IO2IntEnR  (*(unsigned int *)0x400280B0)
+#define IO2IntClr (*(unsigned int *)0x400280AC)	//GPIO interupt clear
 #define ISER0  (*(unsigned int *)0xE000E100)	//Interrupt set-enable register
 
 void magnet0Init() {
 	//set direction
-	FIO0DIR &= ~MAGNET0;
-	FIO0DIR |= 0x00200000;	//set pin 21 to output
+	FI20DIR &= ~MAGNET0;
+	FI20DIR &= ~MAGNET1;
+	FI20DIR |= 0x00200000;	//set pin 21 to output
 
 	//interrupt on rising edge
-	IO0IntEnR |= MAGNET0;
+	IO2IntEnR |= MAGNET0;
+	IO2IntEnR |= MAGNET1;
 
 	//enable external interrupt 3 (for gpio)
 	ISER0 |= 0x200000; //bit 21
 }
 
 void EINT3_IRQHandler() {
-	FIO0PIN ^= 0x00200000;
-
-	IO0IntClr |= MAGNET0;
+	IO2IntClr |= MAGNET0;
+	IO2IntClr |= MAGNET1;
+	FIO2PIN ^= 0x800;
 }
